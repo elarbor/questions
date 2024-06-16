e0ed8e62d45e05b1a52846f5886ba234e5266cf3
全易 <771184240@qq.com>
2024-06-16 19:29:25 +0800

    CommentLayout.vue

---
+++ docs/.vitepress/theme/CommentLayout.vue
@@ -0,0 +1,27 @@
+<!-- Layout.vue -->
+<template>
+  <Layout>
+    <template #doc-after>
+      <giscus-widget
+        id="comments"
+        repo="chergn/questions"
+        repoid="R_kgDOMJ_OGw"
+        category="Announcements"
+        categoryid="DIC_kwDOMJ_OG84CgHg6"
+        mapping="title"
+        term="Welcome to giscus!"
+        reactionsenabled="1"
+        emitmetadata="0"
+        inputposition="top"
+        theme="light"
+        lang="en"
+        loading="lazy"
+      ></giscus-widget>
+    </template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
+import DefaultTheme from "vitepress/theme";
+const { Layout } = DefaultTheme;
+</script>
